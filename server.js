d3269754665f917a801fd23a33b64c40e25a64c5
SayantiMondal <sayantihiya241@gmail.com>
2025-05-14 13:16:04 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+
+const server=require("express")
+const app=server()
+app.get("/home",(req,res)=>{
+    res.send("this is home page")
+})
+
+app.get("/about",(req,res)=>{
+    res.send("this is about page")
+})
+
+app.listen(9000)
